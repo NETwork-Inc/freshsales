c2000663e72bd33c39317799229a28442b31675d
Sivaramakrishnan Narayanan <siva@fyle.in>
2020-01-14 19:35:52 +0530

    Initial skeleton checkin

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='freshsalessdk',
+    version='0.0.1',
+    author='Siva Narayanan',
+    author_email='siva@fyle.in',
+    description='Python SDK for accessing Freshsales',
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords=['freshsales', 'api', 'python', 'sdk'],
+    url='https://github.com/fylein/freshsales-sdk-py',
+    packages=setuptools.find_packages(),
+    install_requires=['requests'],
+    classifiers=[
+        'Topic :: Internet :: WWW/HTTP',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
