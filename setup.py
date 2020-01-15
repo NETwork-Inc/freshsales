bafad7f9262540d2e684eaf8b5e4bcc8d8f6618d
Sivaramakrishnan Narayanan <siva@fyle.in>
2020-01-15 15:16:56 +0530

    Bumped up version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='freshsalessdk',
-    version='0.0.5',
+    version='0.0.6',
     author='Siva Narayanan',
     author_email='siva@fyle.in',
     description='Python SDK for accessing Freshsales',
