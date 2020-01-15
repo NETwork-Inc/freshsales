625878a249599fa68b8e3f308638ce0a700f9379
Sivaramakrishnan Narayanan <siva@fyle.in>
2020-01-15 10:49:54 +0530

    Individual classes normalize their own stuff. Easier to understand this way

--- freshsalessdk/freshsalessdk.py
+++ freshsalessdk/freshsalessdk.py
@@ -14,7 +14,7 @@ class APIBase:
         self.api_key = api_key
         self.default_params = default_params
 
-    def _get(self, path, params={}):
+    def _get_generic(self, path, params={}):
         """Create a HTTP GET request.
 
         Parameters:
@@ -55,18 +55,112 @@ class APIBase:
         return result
 
     def _get_views(self):
-        return self._get(path=f'/{self.resource_type}/filters')['filters']
+        return self._get_generic(path=f'/{self.resource_type}/filters')['filters']
 
     @staticmethod
     def _find_obj_by_id(objs, id):
-#        print(f'trying to find {id} in {objs}')
         for o in objs:
             if o['id'] == id:
                 return o
         return None
 
-    @staticmethod
-    def _normalize(obj, container):
+    def _normalize(self, obj, container):
+        """
+        Every class should normalize it if it wants to do any normalization of the object.
+        E.g. contact object has an owner_id and list of users is in the container. We can fetch
+        the owner object and attach it to the contact object which makes things easier for the client
+        """
+        pass
+
+
+    def _get_all_generator(self, view_id, limit=None):
+        page = 1
+        num = 0
+        while True:
+            start_time = time.time()
+            params = {'page': page}
+            res = self._get_generic(path=f'/{self.resource_type}/view/{view_id}', params=params)
+            total_pages = res['meta']['total_pages']
+            end_time = time.time()
+#            print(f'got page {page} of {total_pages} in {end_time-start_time} seconds')
+        
+            objs = res[self.resource_type]
+            for obj in objs:
+                self._normalize(obj=obj, container=res)
+                num = num + 1
+                if limit and num >= limit:
+                    return
+                yield obj
+
+            page = page + 1
+            if page > total_pages:
+                break
+
+    def _get_by_id(self, id):
+        res = self._get_generic(path=f'/{self.resource_type}/{id}')
+        v = res[self.resource_type_singular]
+        self._normalize(obj=v, container=res)
+        return v
+
+    def get_views(self):
+        return self._get_views()
+
+    def get_all_generator(self, view_id, limit=None):
+        return self._get_all_generator(view_id=view_id, limit=limit)
+
+    def get_all(self, view_id, limit=None):
+        return list(self.get_all_generator(view_id=view_id, limit=limit))
+    
+    def get(self, id):
+        return self._get_by_id(id=id)
+
+
+class Contacts(APIBase):
+    def __init__(self, domain, api_key):
+        default_params = {'include': 'sales_accounts,appointments,owner', 'sort': 'updated_at', 'sort_type': 'desc'}
+        super().__init__(domain=domain, api_key=api_key, resource_type='contacts', default_params=default_params)
+
+    def _normalize(self, obj, container):
+        users = []
+        sales_accounts = []
+        if 'users' in container:
+            users = container['users']
+        if 'sales_accounts' in container:
+            sales_accounts = container['sales_accounts']
+        if 'owner_id' in obj:
+            owner = APIBase._find_obj_by_id(objs=users, id=obj['owner_id'])
+            obj['owner'] = owner
+        if 'sales_account_id' in obj:
+            sales_account = APIBase._find_obj_by_id(objs=sales_accounts, id=obj['sales_account_id'])
+            obj['sales_account'] = sales_account
+
+    def get_activities(self, id):
+        return self._get_generic(f'/contacts/{id}/activities')['activities']
+    
+    def get_appointments(self, id):
+        return self._get_generic(f'/contacts/{id}/appointments')['appointments']
+
+
+class Accounts(APIBase):
+    def __init__(self, domain, api_key):
+        default_params = {'include': 'appointments,owner', 'sort': 'updated_at', 'sort_type': 'desc'}
+        super().__init__(domain=domain, api_key=api_key, resource_type='sales_accounts', default_params=default_params)
+
+    def _normalize(self, obj, container):
+        users = []
+        sales_accounts = []
+        if 'users' in container:
+            users = container['users']
+        if 'owner_id' in obj:
+            owner = APIBase._find_obj_by_id(objs=users, id=obj['owner_id'])
+            obj['owner'] = owner
+
+class Deals(APIBase):
+    def __init__(self, domain, api_key):
+        default_params = {'include': 'sales_account,appointments,owner,deal_stage', 'sort': 'updated_at', 'sort_type': 'desc'}
+        super().__init__(domain=domain, api_key=api_key, resource_type='deals', default_params=default_params)
+
+    def _normalize(self, obj, container):
         users = []
         sales_accounts = []
         deal_stages = []
@@ -85,66 +179,6 @@ class APIBase:
         if 'deal_stage_id' in obj:
             deal_stage = APIBase._find_obj_by_id(objs=deal_stages, id=obj['deal_stage_id'])
             obj['deal_stage'] = deal_stage
-
-
-    def _get_all_generator(self, view_id):
-        page = 1
-        while True:
-            start_time = time.time()
-            params = {'page': page}
-            res = self._get(path=f'/{self.resource_type}/view/{view_id}', params=params)
-            total_pages = res['meta']['total_pages']
-            end_time = time.time()
-#            print(f'got page {page} of {total_pages} in {end_time-start_time} seconds')
-        
-            objs = res[self.resource_type]
-            for obj in objs:
-                APIBase._normalize(obj=obj, container=res)
-                yield obj
-
-            page = page + 1
-            if page > total_pages:
-                break
-
-    def _get_by_id(self, id):
-        res = self._get(path=f'/{self.resource_type}/{id}')
-        v = res[self.resource_type_singular]
-        APIBase._normalize(obj=v, container=res)
-        return v
-
-    def get_views(self):
-        return self._get_views()
-
-    def get_all_generator(self, view_id):
-        return self._get_all_generator(view_id=view_id)
-
-    def get_all(self, view_id):
-        return list(self.get_all_generator(view_id=view_id))
-    
-    def get(self, id):
-        return self._get_by_id(id=id)
-
-
-class Contacts(APIBase):
-    def __init__(self, domain, api_key):
-        default_params = {'include': 'sales_accounts,appointments,owner', 'sort': 'updated_at', 'sort_type': 'desc'}
-        super().__init__(domain=domain, api_key=api_key, resource_type='contacts', default_params=default_params)
-
-    def get_activities(self, id):
-        return self._get(f'/contacts/{id}/activities')['activities']
-
-
-class Accounts(APIBase):
-    def __init__(self, domain, api_key):
-        default_params = {'include': 'appointments,owner', 'sort': 'updated_at', 'sort_type': 'desc'}
-        super().__init__(domain=domain, api_key=api_key, resource_type='sales_accounts', default_params=default_params)
-
-
-class Deals(APIBase):
-    def __init__(self, domain, api_key):
-        default_params = {'include': 'sales_account,appointments,owner,deal_stage', 'sort': 'updated_at', 'sort_type': 'desc'}
-        super().__init__(domain=domain, api_key=api_key, resource_type='deals', default_params=default_params)
-
 
 class FreshsalesSDK:
     def __init__(self, domain, api_key):
